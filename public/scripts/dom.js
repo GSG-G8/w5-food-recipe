202cbfbdf314b39ed7b808b575a08e0797830385
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-02-19 10:47:04 +0200

    client fetch relates #28

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -3,13 +3,15 @@ const precipeDetails = document.querySelector('.food-precipe');
 const foodPrecieClose = document.querySelector('.food-precie__close');
 const foodPrecipeImage = document.querySelector('.food-precipe__image');
 const food = document.querySelector('.food');
+
+const searchInput = document.querySelector(".search__input");
+const searchBtn = document.querySelector(".search__btn");
+
 document.addEventListener("click", e => {
   if (e.target.name == "foodPre") {
     foodPrecipeImage.src = e.target.src;
     precipeDetails.style.display = "flex";
     food.style.display = "none";
-  } else {
-    console.log("no");
   }
 });
 foodPrecieClose.addEventListener('click', e=>{
