427d5a8d9a46b10147ea5638dad8234007ea7572
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-19 09:53:35 +0200

    delete body-parser #26

--- src/router.js
+++ src/router.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-const { Router } = express;
-const router = Router();
+const router = express.Router();
 
 const {
     recipeSearch
 } = require('./controller');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/search', recipeSearch);
-
-app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
+router.use(express.urlencoded({ extended: false }));
+router.post('/search', recipeSearch);
 
 module.exports = router;
