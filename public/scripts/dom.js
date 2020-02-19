4cbe915f7015228cd6c74ea3fb3cb9ae73de0735
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-02-19 15:17:06 +0200

    render details functions relates #35

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -7,19 +7,38 @@ const food = document.querySelector('.food');
 const searchInput = document.querySelector(".search__input");
 const searchBtn = document.querySelector(".search__btn");
 
-document.addEventListener("click", e => {
-  if (e.target.name == "foodPre") {
-    foodPrecipeImage.src = e.target.src;
-    precipeDetails.style.display = "flex";
-    food.style.display = "none";
-  }
-});
-foodPrecieClose.addEventListener('click', e=>{
-food.style.display = "flex";
-precipeDetails.style.display = "none"
-})
+let searchResults = [];
 
-function createRecipeElement(obj) {
+function showDetails() {
+  food.style.display = "none";
+  precipeDetails.style.display = "flex";
+}
+
+function hideDetails() {
+  food.style.display = "flex";
+  precipeDetails.style.display = "none"
+}
+
+foodPrecieClose.onclick = hideDetails;
+
+
+function selectThis() {
+  showDetails()
+  const thisRecipe = searchResults[this.arrayIndex].recipe;
+  foodPrecipeImage.src = thisRecipe.image;
+
+  document.querySelector(".food__name").textContent = thisRecipe.label;
+  const ingerdients = document.querySelector(".ingerdients__ingerden");
+  ingerdients.textContent = "";
+  thisRecipe.ingredientLines.forEach(line => {
+    const el = document.createElement("li");
+    el.textContent = line;
+    ingerdients.appendChild(el);
+  });
+
+}
+
+function createRecipeElement(obj, index) {
   const recipe = obj.recipe;
   
   const article = document.createElement("article");
@@ -32,6 +51,8 @@ function createRecipeElement(obj) {
   img.alt = "food__precipe";
   img.name = "foodPre";
   img.className = "precipe__image";
+  img.arrayIndex = index;
+  img.onclick = selectThis;
   span.textContent = recipe.label;
   span.className = "precipe__name";
   nutrients.className = "precipe__detials";
@@ -54,16 +75,22 @@ function createRecipeElement(obj) {
   return article;
 }
 
+
 function renderSearch(arr) {
   if (!Array.isArray(arr) || arr.length===0) return;
   
+  searchResults = arr;
   food.textContent = '';
-  arr.forEach(recipe => {
-    food.appendChild(createRecipeElement(recipe));
+  arr.forEach((recipe, index) => {
+    food.appendChild(createRecipeElement(recipe, index));
   });
 }
 
 searchInput.oninput = () => {
   searchInput.style.borderColor = "#aaa";
   searchInput.style.backgroundColor = "#fff";
+}
+
+function renderRecipe(obj) {
+  foodPrecipeImage
 }
